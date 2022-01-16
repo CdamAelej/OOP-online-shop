35a763b9666e4e9ecd9edb09552249b790e94c20
Adam Celej <celejadam01@gmail.com>
2022-01-16 23:02:28 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -54,18 +54,18 @@ def zloz_zamowienie():
     wybrany_material = int(input("Wybierz obiekt ktory chcesz zamowic (podaj jego ID): "))
     for item in objects_lists.material_set: #szukam materialu o wybranym id w liscie materialow
         if item.id_number == wybrany_material:
-            if objects_lists.user_set[logged_id].stan_konta >= objects_lists.material_set[wybrany_material].cena: #sprawdzam czy stan konta jest wiekszy od ceny materialu
+            if objects_lists.user_set[logged_id].stan_konta >= item.cena: #sprawdzam czy stan konta jest wiekszy od ceny materialu
                 if item.jest_dostepne: #sprawdzam czy material jest dostepny i zmniejszam dostepna liczbe o jeden
                     item.dostepna_liczba -= 1
                     if item.dostepna_liczba == 0:
                         item.jest_dostepne = False
-                    objects_lists.user_set[logged_id].stan_konta -= objects_lists.material_set[wybrany_material].cena #zmniejszam stan konta o cene materialu
+                    objects_lists.user_set[logged_id].stan_konta -= item.cena #zmniejszam stan konta o cene materialu
                     print("Zamowienie zlozone.\n")
-                    break
+                    return
             else:
                 print("Masz za malo srodkow na koncie. Doladuj konto lub wybierz tanszy produkt.\n")
                 print("\n")
-                break
+                return
     print("Niestety nie mamy produktu o takim ID. Wybierz inny produkt lub wroc do menu.\n")
 
 
@@ -73,8 +73,9 @@ def doladuj_konto():
     kwota = int(input("Podaj kwote ktora chcesz doladowac konto (jesli wpiszesz 0 operacja zostanie przerwana: "))
     while kwota < 0:
         if kwota == 0:
-            break
+            return
         kwota = int(input("Kwota nie moze byc ujemna. Podaj kwote jeszcze raz: "))
+    objects_lists.user_set[logged_id].stan_konta += kwota
 
 
 def menu(state): #  -> object # state to stan zalogowania. Jego wartosc to 0 lub 1 (ewentualnie True lub False)
@@ -93,7 +94,7 @@ def menu(state): #  -> object # state to stan zalogowania. Jego wartosc to 0 lub
     print("4. Zloz zamowienie: ")
     print("5. Anuluj zamowienie: ")
     print("6. Pokaz zamowienia: ")
-    print("7. Doladuj konto: \n")
+    print("7. Doladuj konto: ")
     print("8. Wyjdz z aplikacji: \n")
     wybranaOperacja = int(input())
     if wybranaOperacja == 1:
